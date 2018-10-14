2e8e5e8acd995177bb9e0a0881aa3b8fdd0e7fae
Chat Balinais <chat.balinais@gmail.com>
2018-10-14 11:33:45 +0300

    Clean up App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,29 +4,10 @@ import { Provider } from 'react-redux';
 import store from '../store';
 import AugmentedVideoPlayerContainer from '../containers/AugmentedVideoPlayer';
 
-type Props = {
-  playback: boolean
-};
-
-type State = {};
-
-class App extends React.Component<Props, State> {
-  render() {
-    if (this.player) {
-      const { playback } = this.props;
-      if (playback) {
-        this.player.play();
-      } else {
-        this.player.pause();
-      }
-    }
-
-    return (
-      <Provider store={store}>
-        <AugmentedVideoPlayerContainer />
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <AugmentedVideoPlayerContainer />
+  </Provider>
+);
 
 export default App;
